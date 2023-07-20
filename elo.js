b78a0c46c67f94397a1ba38673900fc7c66b85d3
spaceybread <Spaceybread@gmail.com>
2023-07-21 04:01:14 +0530

    Update elo.js

--- elo.js
+++ elo.js
@@ -1,5 +1,4 @@
 function calculateElo() {
-  // Get values from the input text boxes
   var player1Rating = parseFloat(document.getElementById('r1').value);
   var player2Rating = parseFloat(document.getElementById('r2').value);
   var kValue = parseFloat(document.getElementById('k').value);
@@ -8,6 +7,24 @@ function calculateElo() {
   var expecA = (1 + 10**((player2Rating - player1Rating)/400))**(-1);
   var expecB = (1 + 10**((player1Rating - player2Rating)/400))**(-1);
 
+  // new rating = rating + k * (score - expected score) 
+  // win = 1, draw = 0.5, loss = 0
+  
+  var winAratA = player1Rating + kValue * (1 - expecA);  
+  var winAratB = player2Rating + kValue * (0 - expecB);  
+
+  var winDratA = player1Rating + kValue * (0.5 - expecA);  
+  var winDratB = player2Rating + kValue * (0.5 - expecB);
+
+  var winBratA = player1Rating + kValue * (0 - expecA);  
+  var winBratB = player2Rating + kValue * (1 - expecB);
+
   document.getElementById('expecA').textContent = expecA;
   document.getElementById('expecB').textContent = expecB;
+  document.getElementById('winAratA').textContent = winAratA;
+  document.getElementById('winAratB').textContent = winAratB;
+  document.getElementById('winDratA').textContent = winDratA;
+  document.getElementById('winDratB').textContent = winDratB;
+  document.getElementById('winBratA').textContent = winBratA;
+  document.getElementById('winBratB').textContent = winBratB;
 }
