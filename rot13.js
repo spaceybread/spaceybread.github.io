f3c90dc776c565c900740fb0de6c916824aed9a0
spaceybread <Spaceybread@gmail.com>
2023-07-23 20:48:05 +0530

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -1,7 +1,7 @@
 function rotate() {
 
   var inputText = str=document.getElementById("in").value;
-  inputText.toLowerCase();
+  inputText = inputText.toLowerCase();
   const splitText = inputText.split("");
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 
