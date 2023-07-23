fe71af542fab05360ee3073343d2fe609038292d
spaceybread <Spaceybread@gmail.com>
2023-07-23 20:14:49 +0530

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -2,19 +2,24 @@ function rotate() {
 
   var inputText = str=document.getElementById("in").value;
   const splitText = inputText.split("");
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 
   //look for letter in the alphabet and write that index in the list
   var rotString = "";
 
   for (let i = 0; i < splitText.length; i++) {
-    var aText = splitText[i];
-    var a = alphabet.indexOf(aText);
-    a = a + 1;
-    //nothing but for my sanity while doing mod functions
-    a = a + 13;
-    a = a % 26;
-    a = a - 1;
+
+    if (splitText[i] == " ") {
+        a = 26;
+    } else {
+        var aText = splitText[i];
+        var a = alphabet.indexOf(aText);
+        a = a + 1;
+        //nothing but for my sanity while doing mod functions
+        a = a + 13;
+        a = a % 26;
+        a = a - 1;
+    }
 
     rotString = rotString.concat(alphabet[a]);
   }
