fad8fb77d0390639738f642d300808d5cb515f16
spaceybread <Spaceybread@gmail.com>
2023-07-23 18:55:38 +0530

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -1,7 +1,18 @@
 function rotate() {
 
   var inputText = str=document.getElementById("in").value;
+  const splitText = inputText.split();
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];   
 
-  document.getElementById('out').textContent = inputText;
+  //look for letter in the alphabet and write that index in the list
+
+  let textLength = splitText.length;
+  const textAsNumber = [];
+
+  for (let i = 0; i < textLength; i++) {
+    textAsNumber.push(alphabet.indexOf(splitText[i]));
+  }
+
+  document.getElementById('out').textContent = textAsNumber;
 }
 
